26d499889d63a96cb63425309981efdc54ddd07b
Madeline Browy <browym@ufl.edu>
2023-03-22 21:44:44 -0400

    added menu option 3 and created while loop

--- software.py
+++ software.py
@@ -13,19 +13,29 @@ def encode(password):
     return new_password
 
 
+def decode(new_password):
+    password = [element - 3 for element in new_password]
+    return password
+
+
 def main():
-    menu()
-    choice = int(input('Please enter an option: '))
-    if choice == 1:
-        password = input('Please enter your password to encode: ')
-        password = [int(i) for i in password]
-        encode(password)
-        print('Your password has been encoded and stored!')
-
-    # Choice 2
-
-    if choice == 3:
-        exit()
+    while True:
+        menu()
+        choice = int(input('Please enter an option: '))
+        if choice == 1:
+            password = input('Please enter your password to encode: ')
+            password = [int(i) for i in password]
+            encode(password)
+            print('Your password has been encoded and stored!')
+        if choice == 2:
+            new_password = encode(password)
+            password = decode(new_password)
+            new_password = ''.join(str(i) for i in encode(password))
+            password = ''.join(str(i) for i in password)
+            print(f'The new password is {new_password}, and the original password is {password}.')
+            print()
+        if choice == 3:
+            exit()
 
 
 if __name__ == "__main__":
